60d9e516ac4d7fc0faae96c851dc935e9b98bb95
anuprshetty <anuprshetty@gmail.com>
2020-06-10 22:04:58 +0530

    hash_wallet title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h4>Welcome to Hash Wallet</h4>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 offset-md-3">
+              <form
+                className="gradient my-5 p-2"
+                style={{
+                  borderRadius: "25px",
+                  boxShadow: "1px 1px 15px #000000",
+                }}
+              >
+                <h1 style={{ fontWeight: 900, color: "#FFFFFF" }}>
+                  Hash Wallet
+                </h1>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
